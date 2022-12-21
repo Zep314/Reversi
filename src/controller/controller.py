3ecec781ef6e0f3958f2cc89e8ab9cd05086f763
Dmitry Mordanov <mordanov314@gmail.com>
2022-12-21 16:47:07 +0300

    commit1

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,4 +1,4 @@
-class Game():
+class Controller():
     def __init__(self):
         pass
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from game import Game
+from src.controller.controller import Game
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     myGame = Game()
     myGame.run()
+
+# https://habr.com/ru/company/ruvds/blog/706164/
 
